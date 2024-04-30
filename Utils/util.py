8695d662febc8b559f7c4815f375a5130b42cfa3
Sharath LS <sharath.ls@feuji.com>
2024-04-30 14:36:23 +0530

    pytest selenium

---
+++ Utils/util.py
@@ -0,0 +1,43 @@
+import os
+import random
+import string
+import sys
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
+sys.path.append(parent_dir)
+"""Generates a random string of lowercase letters."""
+
+
+def generate_rand_string(length):
+    characters = string.ascii_lowercase
+    return ''.join(random.choice(characters) for _ in range(length))
+
+
+def analyze_accessibility_results(results):
+    total_violations, critical_violations, moderate_violations, serious_violations = count_violations(results)
+    print("Total violations found:", total_violations)
+    print("Critical violations:", critical_violations)
+    print("Moderate violations:", moderate_violations)
+    print("Serious violations:", serious_violations)
+
+    print_violation_details(results)
+
+
+def print_violation_details(results):
+    if results:
+        print("\nViolation details:")
+        for i, violation in enumerate(results["violations"], start=1):
+            print(f"\nViolation {i}:")
+            print(f"Description: {violation['description']}")
+            print(f"Impact: {violation['impact']}")
+            print(f"Help: {violation['help']}")
+            print(f"Help URL: {violation['helpUrl']}")
+
+
+def count_violations(results):
+    total_violations = len(results["violations"])
+    critical_violations = sum(1 for violation in results["violations"] if violation["impact"] == "critical")
+    moderate_violations = sum(1 for violation in results["violations"] if violation["impact"] == "moderate")
+    serious_violations = sum(1 for violation in results["violations"] if violation["impact"] == "serious")
+    return total_violations, critical_violations, moderate_violations, serious_violations
---
+++ run_tests.py
@@ -0,0 +1,27 @@
+import subprocess
+from concurrent.futures import ThreadPoolExecutor
+from Utils.file_util import ui_test_file, api_test_file, mobile_test_file,accessibility_test_file
+
+
+def run_tests(test_file):
+    subprocess.run(["pytest", test_file, "--alluredir=./allure_results"])
+
+
+# Create a ThreadPoolExecutor with 3 worker threads
+with ThreadPoolExecutor(max_workers=4) as executor:
+    # Submit tasks for each test file
+    futures = [
+        executor.submit(run_tests, ui_test_file),
+        executor.submit(run_tests, api_test_file),
+        executor.submit(run_tests, mobile_test_file),
+        executor.submit(run_tests, accessibility_test_file)
+    ]
+
+    # Wait for all tasks to complete, allowing a timeout for acquiring the lock
+    for future in futures:
+        try:
+            future.result(timeout=3600)  # 1 hour timeout (adjust as needed)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+print("All test files have been executed.")
